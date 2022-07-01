eef3d2d594ad2a3aeb5e221b2fd7f19947ee51a8
Gabriel Henrique <97265255+ghenriquev@users.noreply.github.com>
2022-07-01 19:58:24 -0300

    terceiro commit

--- js/script.js
+++ js/script.js
@@ -3,34 +3,57 @@ function contar() {
     var fim = document.getElementById('fim');
     var passo = document.querySelector('input#passo');
     var res = document.querySelector('div#res');
-    var n1 = Number(inicio.value);
-    var n2 = Number(fim.value);
-    var n3 = Number(passo.value);
+    var i = Number(inicio.value);
+    var f = Number(fim.value);
+    var p = Number(passo.value);
     res.innerHTML = 'Contagem '
 
     if (inicio.value.length == 0 || fim.value.length == 0) {
-        alert('[ERRO] Preencha todos os campos!');
-    } else if (n1 == 0 && n2 == 0){
-        alert('[ERRO] Impossível contar!');
-    } else if (passo.value.length == 0) {
-        alert('[ERRO] O passo será reajustado para 1!');
-        n3 = 1;
-    }
 
-    if (n1 <= n2){
-        res.innerHTML += 'crescente: '
-        for (; n1 <= n2; n1 += n3) {
-            res.innerHTML += `${n1}${String.fromCodePoint(0x1F449)}`
-        }
-        res.innerHTML += `${String.fromCodePoint(0x1F3C1)}`
-    } else if (n2 <= n1) {
-        res.innerHTML += 'decrescente: '
-        for (; n2 <= n1; n1 -= n3){
-            res.innerHTML += `${n1}${String.fromCodePoint(0x1F449)}`
-        }
-        res.innerHTML += `${String.fromCodePoint(0x1F3C1)}`
-    } else {
+        // Início ou fim em branco
+
         res.innerHTML = `Tente novamente. ${String.fromCodePoint(0x1F61F)}`
-    }
+        alert('Complete todos os campos!')
+
+    } else if (i == 0 && f == 0) {
+
+        // Início e fim com o valor 0
+
+        res.innerHTML = `Tente novamente. ${String.fromCodePoint(0x1F61F)}`
+        alert('Impossível contar!')
+
+    } else if (passo.value.length == 0 || p == 0) {
+
+        // Passo em branco ou com valor 0
+
+        alert('[ERRO] O passo será reajustado para 1!');
+        p = 1;
+    } 
     
+    if (i < f) {
+
+        // Contagem crescente
+
+        res.innerHTML += 'crescente: '
+        for (; i <= f; i += p) {
+            res.innerHTML += `${i}${String.fromCodePoint(0x1F449)}` // o emoji pode ser '\u{1F449}'
+        }
+        res.innerHTML += `${String.fromCodePoint(0x1F3C1)}`
+
+    } else if (f < i) {
+
+        // Contagem decrescente
+
+        res.innerHTML += 'decrescente: '
+        for (; f <= i; i -= p) {
+            res.innerHTML += `${i}${String.fromCodePoint(0x1F449)}`
+        }
+        res.innerHTML += `${String.fromCodePoint(0x1F3C1)}`
+
+    } else if (i == f && inicio.value.length != 0 && fim.value.length != 0) {
+
+        // Se os campos de início e fim estiverem preenchidos com valores iguais
+        res.innerHTML = `Início e fim com valores iguais. \u{1F61F}`;
+
+    }
 }
